7a40a0bc9755cfd42361a13600c97836cb1d45be
Risita969 <gianni.adami@outlook.it>
2025-07-02 18:13:42 +0200

    Complete exercise 01

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Inserire un numero intero o la parola 'Uscire' per uscire: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "uscire")
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"Il numero inserito, {numberInt}, è pari!");
+        }
+        else
+        {
+            Console.WriteLine($"Il numero inserito, {numberInt}, è dispari!");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Quello inserito, {numberString}, non è un numero intero!");
+    }
+    
+}while (numberString!.ToLower() != "uscire");
+Console.WriteLine("Game Over.");
+
+
