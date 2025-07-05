8cf373c9f26b269789f658ea92c5ade743530a80
Risita969 <gianni.adami@outlook.it>
2025-07-05 16:03:07 +0200

    Exercise 04 (IsMultipleOfAnother) complete

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,29 @@
+﻿do
+{
+    Console.WriteLine("Inserire due numeri...");
+
+    var a = GetInt("Inserire il primo numero  : ");
+    var b = GetInt("Inserire il secondo numero: ");
+    
+    if (a % b == 0)
+    {
+        Console.WriteLine($"Il numero {a} è multiplo del numero {b}.");
+    }
+    else
+    {
+        Console.WriteLine($"Il numero {a} non è multiplo del numero {b}.");
+    }
+} while (true);
+
+static int GetInt(string message)
+{
+    Console.Write(message);
+    var numberString = Console.ReadLine();
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        return numberInt;
+    }
+    return 0;
+}
+
