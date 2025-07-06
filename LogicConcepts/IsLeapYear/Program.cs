ccf8128d4705621f0773de30fb4b3216140593e0
Risita969 <gianni.adami@outlook.it>
2025-07-06 16:55:37 +0200

    Exercise 05 (IsLeapYear) completed.

---
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,56 @@
+﻿do
+{
+    Console.WriteLine("ANNO BISESTILE");
+
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = GetInt("Inserire l'anno: ");
+
+    if (year == currentYear)
+    {
+        message = "è";
+    }
+    else if (year > currentYear)
+    {
+        message = "sarà";
+    }
+    else
+    {
+        message = "era";
+    }
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"L'anno inserito: {year}, {message} bisestile.");
+            }
+            else
+            {
+                Console.WriteLine($"L'anno inserito: {year}, non {message} bisestile.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"L'anno inserito: {year}, {message} bisestile.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"L'anno inserito: {year}, non {message} bisestile.");
+    }
+} while (true);
+
+static int GetInt(string message)
+{
+    Console.Write(message);
+    var numberString = Console.ReadLine();
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        return numberInt;
+    }
+    return 0;
+}
